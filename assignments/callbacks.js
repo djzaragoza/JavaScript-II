8993244e2190b074db661c92c954cb85a18a95c4
DJ Zaragoza <zaragoza0126@gmail.com>
2019-04-13 13:03:58 -0700

    first commit of project

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -27,7 +27,7 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
-}
+} 
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
